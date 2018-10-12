334fb77abfea50f2cade366b14723cccfe96e7c7
TheLastRar <alastairbarnes123@hotmail.com>
2018-10-12 08:21:20 +0100

    [LINUX BUILD] Fix building on 64bit OS

--- CLR_DEV9_LINUX_MONO/BundleMono.sh
+++ CLR_DEV9_LINUX_MONO/BundleMono.sh
@@ -28,26 +28,32 @@ mkdir $PLUGIN_MONO
 cd $PLUGIN_MONO
 
 # Mono
-apt-get download libmonosgen-2.0-1:i386 mono-runtime-common:i386 libgdiplus:i386
+apt-get download libmonosgen-2.0-1:i386 libmonosgen-2.0-dev:i386 mono-runtime-common:i386 libgdiplus:i386
 
 # Find Files
 LIBMONOSGEN=$(find . -maxdepth 1 -type f -name libmonosgen-2.0-1*)
+LIBMONODEV=$(find . -maxdepth 1 -type f -name libmonosgen-2.0-dev*)
 LIBMGDIPLUS=$(find . -maxdepth 1 -type f -name libgdiplus*)
 LIBMONOCOMMON=$(find . -maxdepth 1 -type f -name mono-runtime-common*)
 
 if [ "$LIBMONOSGEN" = "" ]; then
 	echo "Unpack Mono i386 Failed"
-	RemoveAllPackage $LIBMONOSGEN $LIBMGDIPLUS $LIBMONOCOMMON
+	RemoveAllPackage $LIBMONOSGEN $LIBMONODEV $LIBMGDIPLUS $LIBMONOCOMMON
 	exit 1
 fi
 if [ "$LIBMONOCOMMON" = "" ]; then
 	echo "Unpack Mono i386 Failed"
-	RemoveAllPackage $LIBMONOSGEN $LIBMGDIPLUS $LIBMONOCOMMON
+	RemoveAllPackage $LIBMONOSGEN $LIBMONODEV $LIBMGDIPLUS $LIBMONOCOMMON
 	exit 1
 fi
 if [ "$LIBMGDIPLUS" = "" ]; then
 	echo "Unpack Mono i386 Failed"
-	RemoveAllPackage $LIBMONOSGEN $LIBMGDIPLUS $LIBMONOCOMMON
+	RemoveAllPackage $LIBMONOSGEN $LIBMONODEV $LIBMGDIPLUS $LIBMONOCOMMON
+	exit 1
+fi
+if [ "$LIBMONODEV" = "" ]; then
+	echo "Unpack Mono i386 Failed"
+	RemoveAllPackage $LIBMONOSGEN $LIBMONODEV $LIBMGDIPLUS $LIBMONOCOMMON
 	exit 1
 fi
 
@@ -61,9 +67,12 @@ rm "data.tar.xz"
 ar x $LIBMGDIPLUS "data.tar.xz"
 tar -xf "data.tar.xz"
 rm "data.tar.xz"
+ar x $LIBMONODEV "data.tar.xz"
+tar -xf "data.tar.xz"
+rm "data.tar.xz"
 
 # Cleanup
-RemoveAllPackage $LIBMONOSGEN $LIBMGDIPLUS $LIBMONOCOMMON
+RemoveAllPackage $LIBMONOSGEN $LIBMONODEV $LIBMGDIPLUS $LIBMONOCOMMON
 
 rm "etc" -r
 rm "usr/share" -r
